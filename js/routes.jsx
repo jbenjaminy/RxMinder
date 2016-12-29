c1d8f97d1a52fe9fb1745699ac8e3c3421fa5f18
jbenjaminy <young.benjamin@gmail.com>
2016-12-29 15:34:20 -0500

    adds content to routes.jsx and updates component names

---
+++ js/routes.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import LandingPage from './components/landing-page';
+import NavBar from './components/nav-bar';
+import MedsPage from './components/meds-page';
+import AddMed from './components/add-med';
+import MedDetails from './components/med-details';
+import DoseHistory from './components/doseHistory';
+
+let App = (props) => {
+  return (
+    <div className='app'>
+      <NavBar />
+      <div>
+        {props.children}
+      </div>
+    </div>
+  )
+}
+
+const routes = (
+  <Router history={browserHistory}>
+    <Route path='/' component={App}>
+      <IndexRoute component={LandingPage} />
+      <Route path='/medication' component={MedsPage} />
+      <Route path='/medication/new' component={AddMed} />
+      <Route path='/medication/:medName'>
+        <IndexRoute component={MedDetails} />
+      </Route>
+      <Route path='/medication/history' component={DoseHistory} />
+    </Route>
+  </Router>
+
+);
+
+export default routes;
